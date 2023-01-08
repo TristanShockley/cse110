92bf4a099aae29bfd5938881d5e900510230a12b
Tristan Shockley <tristanshockley@byui.edu>
2023-01-07 18:47:45 -0700

    first commit

---
+++ Favorite Color.py
@@ -0,0 +1,5 @@
+color = input('Please share your favorite color:')
+if color == 'RED' or color == 'Red' or color == 'red':
+    print(' I like the color red too.')
+else:
+    print('I do not like', color, 'I perfer red.')
